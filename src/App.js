945390a58a6991ceeacf9180339ab0e85945ddc3
Junsu Park <pksjmh5295@gmail.com>
2024-03-04 18:18:51 +0900

    fix: 인사말 변경

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Hello World!!!!</code> 안녕하세연?
+          Edit <code>Hello World!!!!</code> 안녕히 가세여~~~
         </p>
         <a
           className="App-link"
